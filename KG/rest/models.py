938ad806c3f6649aa1a7e7a259ba4e0931389ca7
rogonzalez <r.gonzalez9355@yahoo.com>
2013-10-27 15:26:08 -0400

    cleaning stuff

--- KG/rest/models.py
+++ KG/rest/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 # Create your models here.
 class Group(models.Model):
-        level = models.CharField(max_length=100)
-        group = models.CharField(max_length=100)
-        semester = models.CharField(max_length=100)
-        year = models.CharField(max_length=100)
-        #def __unicode__(self):
-         #       return self.group
+        level = models.PositiveIntegerField()
+        group = models.PositiveIntegerField()
+        semester = models.PositiveIntegerField()
+        year = models.PositiveIntegerField()
+        def __unicode__(self):  # Python 3: def __str__(self):
+                return self.group
+        # def __str__(self):
+        #     return "%s %r" % (self.level,self.group)
 
 class Student(models.Model):
 	name = models.CharField(max_length=100)
 	lastname = models.CharField(max_length=100)
-	#group = models.ForeignKey(Group)
-	level = models.CharField(max_length=100)
+	group = models.PositiveIntegerField()
+	level = models.PositiveIntegerField()
 	
 
--- KG/rest/serializers.py
+++ KG/rest/serializers.py
@@ -5,10 +5,10 @@ from rest.models import Student, Group
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Student
-        fields = ('name', 'lastname', 'group')
+        fields = ('name', 'lastname', 'level','group')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        fields = ('grade', 'semester', 'year', 'group')
+        fields = ('level', 'group', 'semester', 'year')
